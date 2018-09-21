2b9ae5c7efaa82a49b381df35bf169574280a528
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-09-21 14:23:03 +0300

    Update and rename test.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <string>
+using json = nlohmann::json;
+int main() {
+json j1 = [
+ ["Si-9.15", "RTS-9.15", "GAZP-9.15"],
+ [100024, 100027, 100050],
+ ["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
+];
+}
