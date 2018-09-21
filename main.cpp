c1a86df500480d7e542a23c7fedd79c501a57331
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-09-21 17:17:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,11 +10,23 @@ json j1 = R"([
 ])"_json;
  std::cout<< j1<< std::endl;
  json str_1;
+ json str_2;
+ json str_3;
   json j_1= j1[0];
   json j_2= j1[1];
   json j_3= j1[2];
   str_1["tiker"] = j_1[0];
   str_1["id"] = j_2[0];
   str_1["description"] = j_3[0];
- std:: cout << str_1<< std::endl;
+  str_2["tiker"] = j_1[1];
+  str_2["id"] = j_2[1];
+  str_2["description"] = j_3[1];
+  str_3["tiker"] = j_1[2];
+  str_3["id"] = j_2[2];
+  str_3["description"] = j_3[2];
+ json result;
+ result.push_back(str_1);
+ result.push_back(str_2);
+ result.push_back(str_3);
+ std::cout<< result<<std::endl;
 }
