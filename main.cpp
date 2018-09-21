c87f1aa9c294a1a35c71459bb0f6403f11987697
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-09-21 17:08:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,4 +9,12 @@ json j1 = R"([
  ["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
 ])"_json;
  std::cout<< j1<< std::endl;
+ json str_1;
+  json j_1= j1[0];
+  json j_2= j1[1];
+  json j_3= j1[2];
+  str_1["tiker"] = j_1[0];
+  str_1["id"] = j_2[0];
+  str_1["description"] = j_3[0];
+ std:: cout << str_1<< std::endl;
 }
