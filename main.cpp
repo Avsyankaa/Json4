c6201184256e4ef641734aa0b0c86808cc384647
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-09-21 16:45:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,9 @@
 #include <string>
 using json = nlohmann::json;
 int main() {
-json j1 = json::array({ {"Si-9.15", "RTS-9.15", "GAZP-9.15"} ,{100024, 100027, 100050}, {"Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"}});
+json j1 = R"([
+ ["Si-9.15", "RTS-9.15", "GAZP-9.15"],
+ [100024, 100027, 100050],
+ ["Futures contract for USD/RUB", "Futures contract for index RTS", "Futures contract for Gazprom shares"]
+])";
 }
